3bfed47a2b35f7d7ed8eace56a5ca71933198a0f
ahumeau <humeau.antoine@gmail.com>
2013-08-23 08:06:31 +0000

    custom module for my personnal aliases

---
+++ modules/antoine/alias.zsh
@@ -0,0 +1,22 @@
+#
+# Defines Git aliases.
+#
+# Authors:
+#   Antoine Humeau <humeau.antoine@gmail.com>
+#
+
+#
+# Processes
+alias j='jobs'
+alias f1='fg 1'
+alias f2='fg 2'
+alias f3='fg 3'
+alias f4='fg 4'
+alias f5='fg 5'
+alias f6='fg 6'
+alias proc="ps aux | grep"
+alias vup="cd /Users/antoine/MinilogsVM/ && vagrant up"
+alias mnl="ssh mnl"
+alias fps="ps aux | grep"
+alias fpip="pip freeze | grep"
+alias c='clear'
